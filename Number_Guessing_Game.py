9db937c97b8ecafd0e1d5219b4071f124c653430
Ayaan Afsar <itsayaanafsar@gmail.com>
2026-01-29 16:51:42 +0530

    Update Number_Guessing_Game.py

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -3,15 +3,15 @@ import random
 attempts = 10
 best_score = None
 print("Welcome to the Number Guessing Game!")
-min = int(input("Enter the minimum number of range: "))
-max = int(input("Enter the maximum number of range: "))
-number_to_guess = random.randint(min, max)
+min_score = int(input("Enter the minimum number of range: "))
+max_score = int(input("Enter the maximum number of range: "))
+number_to_guess = random.randint(min_score, max_score)
 while True:
     try:
         if attempts == 0:
             print(f"You've run out of attempts. The number was {number_to_guess}.")
             break
-        guess = int(input(f"Enter a number between {min} and {max}: "))
+        guess = int(input(f"Enter a number between {min_score} and {max_score}: "))
         if guess == number_to_guess:
             print("Congratulations! You guessed the number.")
             break
@@ -31,4 +31,5 @@ while True:
 if best_score is None or attempts > best_score:
     best_score = attempts
     print(f"New best score: {best_score} attempts remaining!")
+
     
